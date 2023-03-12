bc4415ca293d0ec27003019bd895b0e37b00e3b0
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-12 18:37:46 +0200

    add AppDiv styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,22 +4,33 @@ import { ImageGallery } from "./ImageGallery/ImageGallery";
 import { Loader } from "./Loader/Loader";
 import { Button } from "./Button/Button";
 import { Modal } from "./Modal/Modal";
+import { AppDiv } from "./App.styled";
 
 export class App extends Component {
+  state = {
+    images: [],
+    page: 0,
+  }
 
   loadMore = () => {
     console.log('Load more is clicked');
   };
 
+  componentDidMount() {
+
+  };
+
   render() {
     return (
-      <>
+      <AppDiv>
         <Searchbar />
+
+        
         <Loader />
         <ImageGallery images={[{src:"", alt:"qwerty", id:1},{src:"", alt:"qwerty", id:2}]}/>
         <Button onClick={this.loadMore} />
         <Modal src={"src"} alt={"alt"}/>
-      </>
+      </AppDiv>
     );
   };
 };

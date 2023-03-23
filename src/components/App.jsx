b3cee2a405eac18fe5b518323cdf644053d48b81
teiatan <92535482+teiatan@users.noreply.github.com>
2023-03-23 03:20:36 +0200

    change logic to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useEffect, useState, useRef } from "react";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Searchbar } from "./Searchbar/Searchbar";
@@ -9,55 +9,59 @@ import { Modal } from "./Modal/Modal";
 import { getImages } from "apiService/apiService";
 import { AppDiv } from "./App.styled";
 
-export class App extends Component {
-  state = {
-    searchInputValue: "",
-    foundSearch: "",
-    images: [],
-    page: 1,
-    totalPages: 1,
-    loader: false,
-    loadMoreButton: false,
-    showModal: false,
-    modalImgSrc: "",
-  };
+export function App() {
 
-  async componentDidUpdate(_, prevState) {
-    if(prevState.foundSearch !== this.state.foundSearch || prevState.page !== this.state.page) {
-      
-      this.setState({loader: true})
-      
-      try {
-        const response = await getImages(this.state.searchInputValue, this.state.page);
-
-        if(response.images.length === 0) {
-          toast.error(`These are no "${this.state.foundSearch}" images`);
-        return;
-        };
-        
-        
-        if(this.state.page === 1) {
-          toast.success(`We found ${response.totalHits} images`);
-        };
-        
-        this.setState(prevState => ({
-          images: [...prevState.images, ...response.images], 
-          loadMoreButton: this.state.page < Math.ceil(response.totalHits / 12), 
-        }));
-     
-      } catch (error) {
-        toast.error(error.message);
-        
-      }finally{this.setState({loader: false})}; 
+  const [searchInputValue, setSearchInputValue] = useState("");
+  const [foundSearch, setFoundSearch] = useState("");
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loader, setLoader] = useState(false);
+  const [loadMoreButton, setLoadMoreButton] = useState(false);
+  const [showModal, setShowmodal] = useState(false);
+  const [modalImgSrc, setModalImgSrc] = useState("");
+  const isFirstRender = useRef(true);
+  
+  useEffect(()=>{
+    console.log(isFirstRender);
+    if(isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
     };
+
+    if (foundSearch.trim() !== '') {
+      getImages(foundSearch, page).then(
+        response => {
+          setImages(prevState => [...prevState, ...response.images]);
+          setLoadMoreButton(page < Math.ceil(response.totalHits / 12));
+          
+          if(response.images.length === 0) {
+            toast.error(`These are no "${foundSearch}" images`);
+            return;
+          };
+          
+          if(page === 1) {
+            toast.success(`We found ${response.totalHits} images`);
+          };
+        }
+      ).catch(
+        error => { 
+          toast.error(error.message);
+          console.log(error); 
+        }
+      ).finally(
+        () => {
+          setLoader(false);
+        }
+      );
+      
+    };    
+  },[foundSearch, page]);
+
+  const handleSearchInput = e => {
+    setSearchInputValue(e.currentTarget.value.toLowerCase());
   };
 
-  handleSearchInput = e => {
-    this.setState({searchInputValue: e.currentTarget.value.toLowerCase()});
-  };
-
-  handleSearchSubmit = e => {
-    const {searchInputValue, foundSearch} = this.state;
+  const handleSearchSubmit = e => {
     e.preventDefault();
     window.scrollTo(0,0);
     if (searchInputValue.trim() === '') {
@@ -65,38 +69,38 @@ export class App extends Component {
       return;
     };
     if(searchInputValue !== foundSearch) {
-      this.setState({page:1, images:[], foundSearch:searchInputValue, loadMoreButton: false,});
+      setPage(1);
+      setImages([]);
+      setFoundSearch(searchInputValue);
+      setLoadMoreButton(false);
     };
   };
 
-  loadMore = () => {    
-    this.setState(prevState => {
-      return ({page:prevState.page+1});
-    });
+  const loadMore = () => { 
+    setPage(prevState => prevState+1)
   };
 
-  modalOpen = e => {
+  const modalOpen = e => {
     if(e.target.nodeName === 'IMG') {
-      this.setState({showModal: true, modalImgSrc: e.target.getAttribute("data-modal")})
+      setShowmodal(true);
+      setModalImgSrc(e.target.getAttribute("data-modal"));
     };
   };
 
-  modalClose = () => {
-    this.setState({showModal: false, modalImgSrc: ""});
+  const modalClose = () => {
+    setShowmodal(false);
+    setModalImgSrc("");
   };
 
-  render() {
-    const {searchInputValue, images, loader, loadMoreButton, showModal, modalImgSrc, modalImgAlt} = this.state;
-    return (
-      <AppDiv>  
-        <Searchbar search={searchInputValue} onChange={this.handleSearchInput} onSubmit={this.handleSearchSubmit}/>
-        {images.length !== 0 && (<ImageGallery images={images} modalOpen={this.modalOpen}/>
-        )}
-        {loader === true && <Loader />}
-        {loadMoreButton && <Button onClick={this.loadMore} />}
-        {showModal  && <Modal modalClose={this.modalClose} children={<img src={modalImgSrc} alt={modalImgAlt}/>}/>}
-        <ToastContainer autoClose={2500} />
-      </AppDiv> 
-    );
-  };
+  return (
+    <AppDiv>  
+      <Searchbar search={searchInputValue} onChange={handleSearchInput} onSubmit={handleSearchSubmit}/>
+      {images.length !== 0 && (<ImageGallery images={images} modalOpen={modalOpen}/>
+      )}
+      {loader === true && <Loader />}
+      {loadMoreButton && <Button onClick={loadMore} />}
+      {showModal  && <Modal modalClose={modalClose} children={<img src={modalImgSrc} alt={"modalImgAlt"}/>}/>}
+      <ToastContainer autoClose={2500} />
+    </AppDiv> 
+  );
 };
